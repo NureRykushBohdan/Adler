8b06260188fc8639267d5e8dd53da6e489abee54
NureRykushBohdan <bohdan.rykush@nure.ua>
2025-02-28 18:03:22 +0100

    start

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Налаштування EJS
+app.set("view engine", "ejs");
+
+// Використання папки public для статичних файлів
+app.use(express.static("public"));
+
+// Маршрут для головної сторінки
+app.get("/", (req, res) => {
+    res.render("index", { title: "Головна сторінка" });
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+    console.log(`Сервер працює на http://localhost:${PORT}`);
+});
